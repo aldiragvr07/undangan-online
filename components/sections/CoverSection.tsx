929fdbabb9bc078936b916f0448b34f79b7afb46
aldiragvr07 <aldiragvr07@gmail.com>
2026-05-09 10:41:26 +0700

    zoomimg

--- components/sections/CoverSection.tsx
+++ components/sections/CoverSection.tsx
@@ -17,7 +17,7 @@ export default function CoverSection() {
           preload
           quality={72}
           sizes="(max-width: 480px) 100vw, 480px"
-          className="object-cover object-center"
+          className="object-cover object-center scale-[1.15]"
         />
         {/* Dark gradient overlay - stronger at bottom */}
         <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/30 to-black/20" />
